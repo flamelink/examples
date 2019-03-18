1589ca5b4211649e05e4d8f5d4b7e9a12658c66b
JP Erasmus <jperasmus11@gmail.com>
2019-03-18 21:38:43 +0200

    chore(setup): WIP structure for Next.js blog example

---
+++ examples/nextjs-blog/pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <div>Welcome to the Flamelink + Next.js blog example!</div>
+}
+
+export default Home
---
+++ examples/nextjs-blog/utils/flamelink.js
@@ -0,0 +1,46 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/storage'
+import 'firebase/auth'
+import flamelink from 'flamelink/app'
+import 'flamelink/settings'
+import 'flamelink/content'
+import 'flamelink/storage'
+import 'flamelink/navigation'
+import 'flamelink/users'
+
+let firebaseApp
+
+if (process.browser) {
+  firebaseApp = firebase.apps.length
+    ? firebase.app()
+    : firebase.initializeApp({
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      })
+} else {
+  // Only importing `firebase-admin` here so that it does not end up in the client-side bundle
+  const admin = require('firebase-admin')
+
+  firebaseApp = admin.apps.length
+    ? admin.app()
+    : admin.initializeApp({
+        credential: admin.credential.cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        }),
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+      })
+}
+
+export const flamelinkApp = flamelink({
+  firebaseApp,
+  dbType: 'cf',
+  environment: 'production',
+  locale: 'en-US',
+})
