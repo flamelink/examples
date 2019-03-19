8b2bd7ccd11e082eec851ef5285d703fca6f2b60
Jordan Cole Hunt <jordancolehunt@gmail.com>
2019-03-19 15:28:02 +0200

    outline post

--- examples/nextjs-blog/pages/blog/post.js
+++ examples/nextjs-blog/pages/blog/post.js
@@ -1,12 +1,48 @@
+import { flamelinkApp as app } from '../../utils/flamelink'
+import Markdown from 'markdown-to-jsx'
+import PropTypes from 'prop-types'
+
 const Post = function(props) {
-  return <h1>{props.title}</h1>
+  const {
+    title,
+    date,
+    author: { displayName },
+    content,
+    // image,
+  } = props.post
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{displayName}</h2>
+      <p>{new Date(date).toLocaleDateString()}</p>
+      <Markdown>{content}</Markdown>
+      {/* <img src={}/> */}
+    </div>
+  )
 }
 
 Post.getInitialProps = async ({ query }) => {
   console.log('SLUG', query.slug)
+
+  const [post] = Object.values(
+    (await app.content.getByField({
+      schemaKey: 'blogPost',
+      field: 'slug',
+      value: query.slug,
+      populate: true,
+    })) || {}
+  )
+
+  console.log(post)
+
   return {
-    title: 'blog post title',
+    post,
   }
+}
+
+Post.propTypes = {
+  post: PropTypes.object,
 }
 
 export default Post
