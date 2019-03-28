01807acf1448dc638c2c100122825829a982005b
JP Erasmus <jperasmus11@gmail.com>
2019-03-28 15:59:54 +0200

    feat: dynamically generate a `sitemap.xml` file based on our routes and content

--- examples/nextjs-blog/server.js
+++ examples/nextjs-blog/server.js
@@ -1,15 +1,36 @@
+const express = require('express')
 const next = require('next')
+const fs = require('fs')
 const routes = require('./routes')
-const { createServer } = require('http')
 
+const port = parseInt(process.env.PORT, 10) || 3000
 const app = next({ dev: process.env.NODE_ENV !== 'production' })
 const handler = routes.getRequestHandler(app)
 
-const port = process.env.PORT || 3000
+const { DESTINATION, createSitemap } = require('./sitemap')
 
 app.prepare().then(() => {
-  createServer(handler).listen(port, err => {
+  const server = express()
+
+  // This `server.get()` lets you generate a sitemap on the fly and retrive it from http://your.domain/sitemap.xml
+  // It also create a file if you need to open it with your editor.
+  server.get('/sitemap.xml', function(req, res) {
+    res.header('Content-Type', 'application/xml')
+    ;(async function sendXML() {
+      let xmlFile = await createSitemap()
+      // Send it to the browser
+      res.send(xmlFile)
+      // Create a file on the selected destination
+      fs.writeFileSync(DESTINATION, xmlFile)
+    })()
+  })
+
+  server.get('*', (req, res) => {
+    return handler(req, res)
+  })
+
+  server.listen(port, err => {
     if (err) throw err
-    console.log(`Ready on localhost:${port}`)
+    console.log(`> Ready on http://localhost:${port}`)
   })
 })
