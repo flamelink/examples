94af632b0321c2a15f40e1d9eae6509578002755
JP Erasmus <jperasmus11@gmail.com>
2019-03-28 12:31:49 +0200

    refactor: Post component to use hooks

--- examples/nextjs-blog/pages/blog/post.js
+++ examples/nextjs-blog/pages/blog/post.js
@@ -1,6 +1,7 @@
-import React, { PureComponent } from 'react'
-import Markdown from 'markdown-to-jsx'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import get from 'lodash/get'
+import Markdown from 'markdown-to-jsx'
 import NextError from 'next/error'
 import NextSeo, { ArticleJsonLd } from 'next-seo'
 import { flamelinkApp as app } from '../../utils/flamelink'
@@ -12,175 +13,157 @@ import {
 import { DEFAULT_POST_IMAGE_URL } from '../../constants/constants'
 import { containerInner } from '../../components/styled'
 
-class Post extends PureComponent {
-  constructor(props) {
-    super(props)
-    this._subscription = null
-    this.state = {
-      post: null,
-    }
-  }
+const Post = props => {
+  const [error, setError] = useState(null)
+  const [updatedPost, setPost] = useState(null)
 
-  async componentDidMount() {
+  useEffect(() => {
     const { pathname } = location
     const slug = pathname.split('/').pop()
 
-    this._subscription = app.content.subscribe({
+    return app.content.subscribe({
       schemaKey: 'blogPost',
       changeType: 'modified',
       filters: [['slug', '==', slug]],
       fields: Post.fields,
       populate: Post.populate,
-      callback: (error, response) => {
-        if (error) {
-          throw new Error(
-            'Something went wrong while retrieving all the content. Details:',
-            error
-          )
+      callback: (err, response) => {
+        if (err) {
+          setPost(null)
+          return setError(err)
         }
 
-        const [post] = Object.values(response || {})
+        const [content] = Object.values(response || {})
 
-        this.setState({ post })
+        setError(null)
+        return setPost[content]
       },
     })
-  }
+  }, [])
 
-  componentWillUnmount() {
-    // unsubscribe
-    if (this._subscription) this._subscription()
-  }
+  const { post } = props
 
-  render() {
-    const { post } = this.props
-    const { post: updatedPost } = this.state
+  if (updatedPost || post) {
+    const {
+      title,
+      date,
+      author,
+      content,
+      image,
+      status,
+      seo,
+      tags,
+      _fl_meta_: { createdDate, lastModifiedDate },
+    } = updatedPost || post
 
-    if (updatedPost || post) {
-      const {
-        title,
-        date,
-        author,
-        content,
-        image,
-        status,
-        seo,
-        tags,
-        _fl_meta_: { createdDate, lastModifiedDate },
-      } = updatedPost || post
+    const { displayName } = author
 
-      const { displayName } = author
-
-      // Only show published posts
-      if (status !== 'published') {
-        return <NextError statusCode={404} />
-      }
-
-      const postImage = (image && image[0]) || { url: DEFAULT_POST_IMAGE_URL }
-      const { url } = postImage
-
-      return (
-        <>
-          <header className="banner">
-            <img src={url} alt={getImageAlt(post)} />
-            <div className="post-meta">
-              <p>{`${getDateString(date)} ${displayName}`}</p>
-              <h1>{title}</h1>
-            </div>
-          </header>
-          <div className={containerInner.className}>
-            <Markdown>{content}</Markdown>
-            <style jsx>{`
-              :global(.pageSection) {
-                margin-bottom: 4.28rem;
-              }
-
-              :global(.banner) {
-                width: 100%;
-                position: relative;
-              }
-
-              :global(.banner > img) {
-                width: 100%;
-              }
-
-              :global(.post-meta) {
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                color: #fff;
-              }
-
-              :global(.post-meta > h1) {
-                text-shadow: 0px 0px 10px var(--text-color);
-              }
-
-              :global(.post-meta > p) {
-                text-shadow: 0px 0px 10px var(--text-color);
-              }
-            `}</style>
-            <style jsx>{containerInner}</style>
-          </div>
-          <NextSeo
-            config={{
-              title: seo.ogTitle || seo.metaTitle || title,
-              description: seo.metaDescription,
-              openGraph: {
-                title: seo.ogTitle || seo.metaTitle || title,
-                description: seo.ogDescription || seo.metaDescription,
-                url:
-                  seo.ogUrl || seo.canonicalUrl || typeof window !== 'undefined'
-                    ? window.location.href
-                    : '',
-                type: seo.ogType || 'article',
-                article: {
-                  publishedTime: getDateFromTimestamp(
-                    createdDate
-                  ).toISOString(),
-                  modifiedTime: getDateFromTimestamp(
-                    lastModifiedDate
-                  ).toISOString(),
-                  section: 'Section II',
-                  authors: [
-                    `${
-                      typeof window !== 'undefined'
-                        ? window.location.origin
-                        : ''
-                    }/authors/${author.id}`,
-                  ],
-                  tags,
-                },
-                images: [
-                  {
-                    url: seo.ogImage || url,
-                    alt: seo.ogTitle || title,
-                  },
-                ],
-              },
-            }}
-          />
-          <ArticleJsonLd
-            url={url}
-            title={seo.metaTitle || title}
-            images={[url]}
-            datePublished={getDateFromTimestamp(createdDate).toISOString()}
-            dateModified={getDateFromTimestamp(lastModifiedDate).toISOString()}
-            authorName={displayName}
-            publisherName="Flamelink"
-            publisherLogo="https://www.example.com/photos/logo.jpg"
-            description={seo.metaDescription}
-          />
-        </>
-      )
+    // Only show published posts
+    if (status !== 'published') {
+      return <NextError statusCode={404} />
     }
 
-    return <NextError statusCode={404} />
+    const postImage = (image && image[0]) || { url: DEFAULT_POST_IMAGE_URL }
+    const { url } = postImage
+
+    return (
+      <>
+        <header className="banner">
+          <img src={url} alt={getImageAlt(post)} />
+          <div className="post-meta">
+            <p>{`${getDateString(date)} ${displayName}`}</p>
+            <h1>{title}</h1>
+          </div>
+        </header>
+        <div className={containerInner.className}>
+          <Markdown>{content}</Markdown>
+          <style jsx>{`
+            :global(.pageSection) {
+              margin-bottom: 4.28rem;
+            }
+
+            :global(.banner) {
+              width: 100%;
+              position: relative;
+            }
+
+            :global(.banner > img) {
+              width: 100%;
+            }
+
+            :global(.post-meta) {
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+              color: #fff;
+            }
+
+            :global(.post-meta > h1) {
+              text-shadow: 0px 0px 10px var(--text-color);
+            }
+
+            :global(.post-meta > p) {
+              text-shadow: 0px 0px 10px var(--text-color);
+            }
+          `}</style>
+          <style jsx>{containerInner}</style>
+        </div>
+        <NextSeo
+          config={{
+            title: seo.ogTitle || seo.metaTitle || title,
+            description: seo.metaDescription,
+            openGraph: {
+              title: seo.ogTitle || seo.metaTitle || title,
+              description: seo.ogDescription || seo.metaDescription,
+              url:
+                seo.ogUrl || seo.canonicalUrl || typeof window !== 'undefined'
+                  ? window.location.href
+                  : '',
+              type: seo.ogType || 'article',
+              article: {
+                publishedTime: getDateFromTimestamp(createdDate).toISOString(),
+                modifiedTime: getDateFromTimestamp(
+                  lastModifiedDate
+                ).toISOString(),
+                section: 'Section II',
+                authors: [
+                  `${
+                    typeof window !== 'undefined' ? window.location.origin : ''
+                  }/authors/${author.id}`,
+                ],
+                tags,
+              },
+              images: [
+                {
+                  url: seo.ogImage || url,
+                  alt: seo.ogTitle || title,
+                },
+              ],
+            },
+          }}
+        />
+        <ArticleJsonLd
+          url={url}
+          title={seo.metaTitle || title}
+          images={[url]}
+          datePublished={getDateFromTimestamp(createdDate).toISOString()}
+          dateModified={getDateFromTimestamp(lastModifiedDate).toISOString()}
+          authorName={displayName}
+          publisherName="Flamelink"
+          publisherLogo="https://www.example.com/photos/logo.jpg"
+          description={seo.metaDescription}
+        />
+      </>
+    )
   }
+
+  return <NextError statusCode={get(error, 'code', 404)} />
 }
 
 Post.populate = [
@@ -188,6 +171,7 @@ Post.populate = [
     field: 'author',
     fields: ['displayName', 'id'],
   },
+  'image',
 ]
 
 Post.fields = [
